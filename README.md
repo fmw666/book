3ab4573a489d92170b70555c7ce96a37eaa42409
fmw666 <fmw19990718@qq.com>
2022-06-30 00:17:03 +0800

    publish book

---
+++ README.md
@@ -0,0 +1,6 @@
+### 说明文档
+
+该分支用于存储 gitbook 源文件。
+
+通过该分支 ./deploy.sh 可以将文档部署到 gh-pages 分支。
+> github pages 部署在 gh-pages 分支上
---
+++ deploy.bat
@@ -0,0 +1,39 @@
+@REM 临时目录名变量
+set TEMP_DIR=%tmp_gitbook_0718%
+
+@REM 判断临时目录是否存在，如果存在则删除目录
+if exist %TEMP_DIR% rmdir /s /q %TEMP_DIR%
+
+@REM 创建临时目录
+mkdir %TEMP_DIR%
+
+@REM 通过 gitbook 命令进行编译
+gitbook build
+
+@REM 将 _book 下所有文件复制到临时目录
+xcopy /s /y _book %TEMP_DIR%
+
+@REM 切换到 gh-pages 分支
+git checkout -b gh-pages
+
+@REM 清空该分支下的所有文件
+git rm -rf .
+
+@REM 将临时目录中的文件复制到该分支
+xcopy /s /y %TEMP_DIR% .
+cp -r $TEMP_DIR/* .
+
+# 提交到本地仓库
+git add .
+
+# 提交到远程仓库
+git commit -m "publish book"
+
+# 推送到远程仓库
+git push origin gh-pages
+
+# 切换回主分支
+git checkout main
+
+# 删除临时目录
+rm -rf $TEMP_DIR
